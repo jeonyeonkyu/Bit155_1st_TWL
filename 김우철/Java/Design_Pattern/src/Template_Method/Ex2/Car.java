991ce1ad9bee016dcca5127fb6db5006bed516d3
jikimee64 <jikimee64@gmail.com>
2020-04-08 20:49:40 +0900

    우철 : 깃, 디자인 패턴 반영

--- 김우철/Java/Design_Pattern/src/Template_Method/Ex2/Car.java
+++ 김우철/Java/Design_Pattern/src/Template_Method/Ex2/Car.java
@@ -18,7 +18,6 @@ public abstract class Car {
 	//알고리즘에서 필수적이지 않은 부분으로서 재정의해도 되고 안해도 됨.
 	//공백이거나 기본 행동을 정의
 	//서브 클래스에서 사용하고 싶을 때만 오버라이딩 (강제성 X)
-	//이번 예제에서는 서브클래스에 오버라이딩.
 	void runSomething() {
 		System.out.println("브레이크");
 	}
