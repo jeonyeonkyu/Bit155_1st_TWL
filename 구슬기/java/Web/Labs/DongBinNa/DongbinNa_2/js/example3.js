04981c7bac8e81b45343b4dc0f452cc07278287d
ksks723 <ksks723@gmail.com>
2020-04-19 02:01:06 +0900

    슬기 : 연규동영상부트스트랩 공부..

---
+++ 구슬기/java/Web/Labs/DongBinNa/DongbinNa_2/js/example3.js
@@ -0,0 +1,5 @@
+var button= document.getElementById('button');
+
+    button.addEventListener('click', function(event){
+        alert('버튼을 클릭하셨습니다.');
+    });
---
+++ 구슬기/java/Web/Labs/DongBinNa/DongbinNa_2/js/example4.js
@@ -0,0 +1,13 @@
+function buttonClickEvent(event){
+    switch(event.target.id){
+        case 'button1':
+            alert('버튼 1');
+            break;
+        
+        case 'button2':
+            alert('버튼 2');
+            break;
+            
+    }
+
+}
