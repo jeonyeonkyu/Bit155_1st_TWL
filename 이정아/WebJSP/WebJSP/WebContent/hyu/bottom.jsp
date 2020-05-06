9d6b05fff567d13538ba2830456e1c9abcc46b14
jjung <junga710@naver.com>
2020-05-06 10:34:00 +0900

    정아 : 푸시

--- 이정아/WebJSP/WebJSP/WebContent/hyu/bottom.jsp
+++
@@ -1,3 +0,0 @@
-  <section class="mb-3 pt-4 pb-5">
-        <div class="text-center">Copyright By 2020-JungahLee</div>
-    </section>
--- 이정아/WebJSP/WebServlet_1/src/com/bit/Message.java
+++
@@ -1,7 +0,0 @@
-package com.bit;
-
-public class Message {
-	public String getMessage(String str) {
-		return str + "Model2 base MVC";
-	}
-}
